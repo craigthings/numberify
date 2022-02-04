c93cfc3e7ce478d353bc906c9c4bd84deb152877
Craig <me@craigalbert.com>
2022-02-04 14:27:00 -0500

    added in proper font handling. added in naming of frames. renaming to numberify

--- code.js
+++ code.js
@@ -7,28 +7,35 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 300, height: 153 });
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
-    yield figma.loadFontAsync({ family: "Trade Gothic Next LT Pro", style: "Regular" });
     let frames = figma.currentPage.children.filter(node => node.type === "FRAME");
-    // console.log(frames);
+    console.log(msg);
     frames.sort((frameA, frameB) => frameA.x > frameB.x ? 1 : -1);
-    frames.forEach((frame, i) => {
+    let frameIndex = 0;
+    for (let frame of frames) {
+        frameIndex++;
+        if (msg.numberFramesEnabled)
+            frame.name = `${msg.prefix}${frameIndex}`;
         let node = frame;
         let textFields = node.findAll(node => {
             return node.type === "TEXT";
         });
-        textFields.forEach((textField) => __awaiter(this, void 0, void 0, function* () {
+        for (let textField of textFields) {
             textField = textField;
             let fontName = textField.fontName;
-            // await figma.loadFontAsync(fontName);
-            if (textField.name.indexOf('{p#}') > -1)
-                textField.characters = String(i + 1);
-        }));
-    });
+            if (!fontName.family)
+                continue;
+            if (textField.name.indexOf('{p#}') > -1) {
+                console.log(fontName);
+                yield figma.loadFontAsync(fontName);
+                textField.characters = String(frameIndex);
+            }
+        }
+    }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
     figma.closePlugin();
--- code.ts
+++ code.ts
@@ -1,26 +1,35 @@
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 300, height: 153 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = async msg => {
-    await figma.loadFontAsync({ family: "Trade Gothic Next LT Pro", style: "Regular" });
     let frames = figma.currentPage.children.filter(node => node.type === "FRAME");
     
-    // console.log(frames);
+    console.log(msg);
     frames.sort((frameA, frameB) => frameA.x > frameB.x ? 1 : -1);
-    frames.forEach((frame, i) => {
+    let frameIndex = 0;
+    for(let frame of frames) {
+      frameIndex++;
+
+      if(msg.numberFramesEnabled) frame.name = `${msg.prefix}${frameIndex}`;
+
       let node = frame as unknown as PageNode;
       let textFields = node.findAll(node => {
         return node.type === "TEXT";
       });
-      textFields.forEach(async textField => {
+      
+      for(let textField of textFields) {
         textField = textField as TextNode;
         let fontName = textField.fontName as FontName;
-        // await figma.loadFontAsync(fontName);
-        if(textField.name.indexOf('{p#}') > -1) textField.characters = String(i + 1);
-      });
-    })
+        if(!fontName.family) continue;
+        if(textField.name.indexOf('{p#}') > -1) {
+          console.log(fontName);
+          await figma.loadFontAsync(fontName);
+          textField.characters = String(frameIndex);
+        }
+      }
+    }
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
   figma.closePlugin();
